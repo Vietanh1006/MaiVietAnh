ae7e8fa76f7e1ce5b3e8982315a7ced6755a9ed0
Vietanh1006 <maivietanh1006@gmail.com>
2021-10-07 07:58:57 +0700

    day code

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
 using RazorPagesMovie.Data;
@@ -19,7 +20,11 @@ namespace RazorPagesMovie.Pages_Movies
         }
 
         public IList<Movie> Movie { get;set; }
-
+         [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
         public async Task OnGetAsync()
         {
             Movie = await _context.Movie.ToListAsync();
